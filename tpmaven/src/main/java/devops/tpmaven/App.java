7e268297d4f82132330e4ebadd0d39a29035c832
jonathan.fernando-johnson <jonathan.fernando-johnson@mathinfo.i-univ-tlse2.fr>
2018-10-04 14:27:29 +0200

    ajout projet tpmaven

---
+++ tpmaven/src/main/java/devops/tpmaven/App.java
@@ -0,0 +1,13 @@
+package devops.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
